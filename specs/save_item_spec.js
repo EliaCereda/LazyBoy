7d0b40115816712c42cd50e4ddd9ac5fb6b219df
Elia Cereda <eliacereda@gmail.com>
2017-05-19 10:26:13 +0200

    Improved some tests in save_item_spec.js and remove_spec.js

--- specs/save_item_spec.js
+++ specs/save_item_spec.js
@@ -18,13 +18,15 @@ describe("Saving Item", function () {
   it("Should save item into db", function (done) {
 
     var user = Model('User').create({
-      id : "EddieVedder",
+      _id : "EddieVedder",
       name : "Eddie",
       surname : "Vedder"
     });
 
     user.save(function (err, result) {
       if (err) throw err;
+
+      user.id.should.equal("EddieVedder");
 
       db.get(user.id, function (err, loaded_user) {
         loaded_user.name.should.equal(user.name);
@@ -37,7 +39,6 @@ describe("Saving Item", function () {
 
   it("Should create id for model if not defined", function (done) {
     Model.define('User_no_id', {
-      id: String,
       name: String,
       surname: String,
     });
@@ -55,11 +56,12 @@ describe("Saving Item", function () {
 
   it("Should have a model type", function (done) {
     var user = Model('User').create({
-      id : "Jimmy",
+      _id : "Jimmy",
       name : "Jimmy",
       surname : "Page"
     });
 
+    user.id.should.equal("Jimmy");
     user.model_type.should.equal("User");
 
     user.save(function (err, result) {
@@ -68,7 +70,7 @@ describe("Saving Item", function () {
         throw err;      
       }
 
-      db.get(user.id,function (err, loaded_user) {
+      db.get(user.id, function (err, loaded_user) {
         loaded_user.model_type.should.equal("User");
         done();
       });
@@ -79,7 +81,7 @@ describe("Saving Item", function () {
   it("Should have a created date", function (done) {
 
     var user = Model('User').create({
-      id : "Jimi",
+      _id : "Jimi",
       name : "Jimi",
       surname : "Hendrix"
     });
@@ -101,20 +103,22 @@ describe("Saving Item", function () {
 
   it("Should return a json version of model", function () {
     var user = Model('User').create({
-      id : "Jimi",
+      _id : "Jimi",
       name : "Jimi",
       surname : "Hendrix"
     });
 
-    var attr = {
-      id : "Jimi",
-      name : "Jimi",
-      surname : "Hendrix"
+    var attr = { 
+      model_type: 'User',
+      id: 'Jimi',
+      name: 'Jimi',
+      surname: 'Hendrix',
+
+      dateCreated: undefined,
+      lastUpdated: undefined,
     };
 
-    var user_json = user.toJSON()
-    user_json.name.should.equal(attr.name);
-    user_json.surname.should.equal(attr.surname);
+    user.toJSON().should.deepEqual(attr);
   })
 });
 
